db07f67447b3f497cf69076489ff5c24d95836f0
y6nH <y6nH@live.co.uk>
2018-12-13 00:43:34 +0000

    Solved 5a

--- src/day5.rs
+++ src/day5.rs
@@ -1,23 +1,44 @@
 use crate::util;
 
 pub fn a() {
-  let s: String = util::read_file_to_string("input4");
-  let bytes = s.as_bytes();
+  let mut s: String = util::read_file_to_string("input5");
+  unsafe {
+    let bytes = s.as_mut_vec().to_vec();
+    let chomped = chomp_bytes(bytes);
+    println!("5a: {}", chomped.len());
+    //println!("{}", String::from_utf8(chomped).unwrap());
+  }
 }
 
-fn chomp_bytes(bytes: &[u8]) -> &[u8] {
+fn chomp_bytes(bytes: Vec<u8>) -> Vec<u8> {
   let l = bytes.len();
+  let mut result: Vec<u8> = Vec::new();
+  let mut maybe_ok: u8 = bytes[0];
 
-  let mut result: &[u8] = &[];
+  for w in bytes.windows(2) {
+    // The difference between the codes of lowercase and capital letters is 32.
+    // Using wrapping subtraction logic, 224 indicates a difference of 32 in the other direction.
+    let diff = w[1].wrapping_sub(w[0]);
 
-  for i in 1..l {
-    let diff = bytes[i].wrapping_sub(bytes[i - 1]);
-    match diff {
-      32 => {}                        // aA
-      224 => {}                       // Aa
-      _ => result.push(bytes[i - 1]), // TODO
+    if maybe_ok > 0 && (diff == 32 || diff == 224) {
+      maybe_ok = 0;
+    } else {
+      if maybe_ok > 0 {
+        result.push(maybe_ok);
+      }
+      maybe_ok = w[1];
     }
+    // println!("result {:?}", result);
+  }
+  if maybe_ok != 0 {
+    result.push(maybe_ok);
   }
 
-  return result;
+  // Recurse until it stops getting smaller
+  if result.len() == l {
+    return result;
+  } else {
+    // println!("Length: {}", result.len());
+    return chomp_bytes(result);
+  }
 }
