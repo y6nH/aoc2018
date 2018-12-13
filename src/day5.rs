4ffac3233560fa0ab9d889c2b99db19c2d626c0a
Hugh Spiller <hs135083@renishaw.com>
2018-12-13 13:20:18 +0000

    Solved 5B

--- src/day5.rs
+++ src/day5.rs
@@ -4,9 +4,8 @@ pub fn a() {
   let mut s: String = util::read_file_to_string("input5");
   unsafe {
     let bytes = s.as_mut_vec().to_vec();
-    let collapsed = collapse_bytes(bytes);
-    println!("5a: {}", collapsed.len());
-    //println!("{}", String::from_utf8(chomped).unwrap());
+    let collapsed = collapse_bytes_loop(bytes);
+    println!("5A: {}", collapsed.len());
   }
 }
 
@@ -14,10 +13,29 @@ pub fn b() {
   let mut s: String = util::read_file_to_string("input5");
   // Find the smallest collapsed size resulting from removing one type of unit
 
+  let mut smallest = s.len();
+
+  for c in 65u8..=90u8 {
+    unsafe {
+      let mut bytes = s.as_mut_vec().to_vec();
+      bytes.retain(|&x| x != c && x != c + 32);
+
+      let collapsed = collapse_bytes_loop(bytes);
+      if collapsed.len() < smallest {
+        smallest = collapsed.len();
+      }
+      // println!(
+      //   "{}: {}",
+      //   String::from_utf8(vec![c, c + 32]).unwrap(),
+      //   collapsed.len()
+      // );
+    }
+  }
+  println!("5B: {}", smallest);
 }
 
-fn collapse_bytes(bytes: Vec<u8>) -> Vec<u8> {
-  let l = bytes.len();
+fn collapse_bytes(bytes: &Vec<u8>) -> Vec<u8> {
+  // let l = bytes.len();
   let mut result: Vec<u8> = Vec::new();
   let mut maybe_ok: u8 = bytes[0];
 
@@ -40,11 +58,26 @@ fn collapse_bytes(bytes: Vec<u8>) -> Vec<u8> {
     result.push(maybe_ok);
   }
 
-  // Recurse until it stops getting smaller
-  if result.len() == l {
-    return result;
-  } else {
-    // println!("Length: {}", result.len());
-    return collapse_bytes(result);
+  return result;
+
+  // Recursive strategy hit a stach overflow on part B!
+  // if result.len() == l {
+  //   return result;
+  // } else {
+  //   // println!("Length: {}", result.len());
+  //   return collapse_bytes(result);
+  // }
+}
+
+fn collapse_bytes_loop(bytes: Vec<u8>) -> Vec<u8> {
+  let mut length = bytes.len();
+  let mut result = bytes;
+
+  loop {
+    result = collapse_bytes(&result);
+    if result.len() == length {
+      return result;
+    }
+    length = result.len();
   }
 }
